318212cbd40c44b33bf4dcac4e43a9070df226ee
Nikolaj J-K <kagenogmig@gmail.com>
2021-05-12 20:00:47 +0200

    Update chartScript.js

--- website/chartScript.js
+++ website/chartScript.js
@@ -1,35 +1,69 @@
+function getWeightFromServer(before) {
+    var labels = [];
+    var dataForChart = [];
+
+    fetch(`http://127.0.0.1:8080/weight/${before}`, {
+    })
+        .then(response => response.json())
+        .then(data => {
+            for (let element of data) {
+                let timestamp = element.insertedon;
+                timestamp = moment(parseInt(timestamp));
+                labels.push(timestamp);
+
+                let value = element.weightvalue;
+                dataForChart.push(value);
+            }
+
+            myChart.data.labels = labels;
+            myChart.data.datasets[0].data = dataForChart;
+            myChart.update();
+        });
+}
+
+var buttonWeek = document.getElementById("button-week");
+var buttonMonth = document.getElementById("button-month");
+var buttonYear = document.getElementById("button-year");
+
+buttonWeek.addEventListener('click', (event) => {
+    getWeightFromServer('week');
+});
+buttonMonth.addEventListener('click', (event) => {
+    getWeightFromServer('month');
+});
+buttonYear.addEventListener('click', (event) => {
+    getWeightFromServer('year');
+});
+
+getWeightFromServer('week');
+
 var ctx = document.getElementById('weightChart').getContext('2d');
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Dit kæledyrs vægt over tid',
+            data: []
         }]
     },
     options: {
         scales: {
             y: {
                 beginAtZero: true
+            },
+            x: {
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        hour: 'ddd HH:mm',
+                        day: 'ddd DD/MM'
+                    },
+                    minUnit: 'hour',
+                }
             }
         }
     }
 });
+
