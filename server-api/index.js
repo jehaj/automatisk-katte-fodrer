3603041bbf9fc11298b1c8c1998d76166304349f
Nikolaj J-K <kagenogmig@gmail.com>
2021-05-19 21:10:42 +0200

    Deployed stuff

--- server-api/index.js
+++ server-api/index.js
@@ -7,7 +7,8 @@ const morgan = require('morgan');
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 client.connect()
